b0a2a275491bd371ada8db7954f8abc0f507bc6a
Nixon Andhika <43450143+hidx1@users.noreply.github.com>
2018-11-01 14:44:59 +0700

    Update game_mechanic.h

--- game_mechanic.h
+++ game_mechanic.h
@@ -2,4 +2,10 @@
 /* File header yang berisi fungsi/prosedur untuk game mechanic yang membutuhkan
 gabungan dari beberapa ADT */
 
-void CreateFood();
+#include "kamus.h"
+
+void CreateFood(Stack * Hand, Stack * Tray,  Bintree Resep);
+/* Prosedur untuk Pop semua isi Stack Hand, membandingkan dengan tree, dan
+kemudian membuat makanan dengan Push ke Stack Tray*/
+/* I.S. Stack terdefinisi, Tree terdefinisi */
+/* F.s. Stack Tray terisi dengan daun makanan dari Tree, Stack Hand kosong */
