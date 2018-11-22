c62de162c8d401931fe03e1bbc18611e9c284970
vincent VB <42698353+vincentVB14@users.noreply.github.com>
2018-11-22 21:13:12 +0700

    nebus dosa

--- Array/modarray.c
+++ Array/modarray.c
@@ -9,7 +9,7 @@
 
 /* ********** KONSTRUKTOR ********** */
 /* Konstruktor : create tabel kosong  */
-void MakeEmpty (TabInt *T)
+void MakeEmptyArray (TabInt *T)
 /* I.S. T sembarang */
 /* F.S. Terbentuk tabel T kosong dengan kapasitas IdxMax-IdxMin+1 */
 {
@@ -21,36 +21,7 @@ void MakeEmpty (TabInt *T)
 }
 
 /* ********** SELEKTOR (TAMBAHAN) ********** */
-/* *** Banyaknya elemen *** */
-int NbElmt (TabInt T)
-/* Mengirimkan banyaknya elemen efektif tabel */
-/* Mengirimkan nol jika tabel kosong */
-/* *** Daya tampung container *** */
-{
-	//KAMUS LOKAL
-	int nilai;
-	
-	//ALGORITMA
-	if (Neff(T) = 0)
-	{
-		nilai = 0;
-	}
-	else
-	{
-		nilai = Neff(T)
-	}
-	return (nilai);
-}
-int MaxNbEl (TabInt T)
-/* Mengirimkan maksimum elemen yang dapat ditampung oleh tabel */
-/* *** Selektor INDEKS *** */
-{
-	//KAMUS LOKAL
-	
-	//ALGORITMA
-	return (IdxMax - Neff(T));
-}
-IdxType GetFirstIdx (TabInt T)
+IdxType GetFirstIdxArray (TabInt T)
 /* Prekondisi : Tabel T tidak kosong */
 /* Mengirimkan indeks elemen T pertama */
 {
@@ -59,7 +30,7 @@ IdxType GetFirstIdx (TabInt T)
 	//ALGORITMA
 	return (IdxMin);
 }
-IdxType GetLastIdx (TabInt T)
+IdxType GetLastIdxArray (TabInt T)
 /* Prekondisi : Tabel T tidak kosong */
 /* Mengirimkan indeks elemen T terakhir */
 {
@@ -69,29 +40,9 @@ IdxType GetLastIdx (TabInt T)
 	return (Neff(T));	
 }
 
-/* ********** Test Indeks yang valid ********** */
-boolean IsIdxValid (TabInt T, IdxType i)
-/* Mengirimkan true jika i adalah indeks yang valid utk ukuran tabel */
-/* yaitu antara indeks yang terdefinisi utk container*/
-{
-	//KAMUS LOKAL
-	
-	//ALGORITMA
-	return ((i >= GetFirstIdx(T)) && (i <= IdxMax));
-}
-boolean IsIdxEff (TabInt T, IdxType i)
-/* Mengirimkan true jika i adalah indeks yang terdefinisi utk tabel */
-/* yaitu antara FirstIdx(T)..LastIdx(T) */
-{
-	//KAMUS LOKAL
-	
-	//ALGORITMA
-	return ((i >= GetFirstIdx(T)) && (i <= GetLastIdx(T)));
-}
-
 /* ********** TEST KOSONG/PENUH ********** */
 /* *** Test tabel kosong *** */
-boolean IsEmpty (TabInt T)
+boolean IsEmptyArray (TabInt T)
 /* Mengirimkan true jika tabel T kosong, mengirimkan false jika tidak */
 /* *** Test tabel penuh *** */
 {
@@ -100,7 +51,7 @@ boolean IsEmpty (TabInt T)
 	//ALGORITMA
 	return (Neff(T) == 0);
 }
-boolean IsFull (TabInt T)
+boolean IsFullArray (TabInt T)
 /* Mengirimkan true jika tabel T penuh, mengirimkan false jika tidak */
 {
 	//KAMUS LOKAL
@@ -109,129 +60,7 @@ boolean IsFull (TabInt T)
 	return (Neff(T) == IdxMax);
 }
 
-/* ********** BACA dan TULIS dengan INPUT/OUTPUT device ********** */
-/* *** Mendefinisikan isi tabel dari pembacaan *** */
-void BacaIsi (TabInt *T)
-/* I.S. T sembarang */
-/* F.S. Tabel T terdefinisi */
-/* Proses : membaca banyaknya elemen T dan mengisi nilainya */
-/* 1. Baca banyaknya elemen diakhiri enter, misalnya N */
-/*    Pembacaan diulangi sampai didapat N yang benar yaitu 0 <= N <= MaxNbEl(T) */
-/*    Jika N tidak valid, tidak diberikan pesan kesalahan */
-/* 2. Jika 0 < N <= MaxNbEl(T); Lakukan N kali: Baca elemen mulai dari indeks 
-      IdxMin satu per satu diakhiri enter */
-/*    Jika N = 0; hanya terbentuk T kosong */
-{
-	//KAMUS LOKAL
-	int N;
-	int i;
-	
-	//ALGORITMA
-	scanf ("%d", &N);
-	while ((N < 0) && (N > MaxNbEl(*T))) 
-	{
-		scanf ("%d", &N);
-	}
-	if (N == 0)
-	{
-		Neff(*T) = Neff(*T);
-	}
-	else
-	{
-		for (i = 1; i <= N; i++)
-		{
-			scanf ("&d", &isi);
-			TI(*T)[i] = isi;
-			Neff(*T) += 1;
-		}
-	}
-}
-void BacaIsiTab (TabInt *T)
-/* I.S. T sembarang */
-/* F.S. Tabel T terdefinisi */
-/* Proses : membaca elemen T sampai dimasukkan nilai -9999 */
-/* Dibaca elemen satu per satu dan disimpan mulai dari IdxMin */
-/* Pembacaan dihentikan jika pengguna memasukkan nilai -9999 */
-/* Jika dari pertama dimasukkan nilai -9999 maka terbentuk T kosong */
-{
-	//KAMUS LOKAL
-	
-	//ALGORITMA
-	
-}
-void TulisIsi (TabInt T)
-/* Proses : Menuliskan isi tabel dengan traversal */
-/* I.S. T boleh kosong */
-/* F.S. Jika T tidak kosong : indeks dan elemen tabel ditulis berderet ke bawah */
-/*      Jika T kosong : Hanya menulis "Tabel kosong" */
-/* Contoh: Jika isi Tabel: [1, 20, 30, 50]
-   Maka tercetak di layar:
-   [1]1
-   [2]20
-   [3]30
-   [4]50 */
-{
-	//KAMUS LOKAL
-	
-	//ALGORITMA
-	
-}
-void TulisIsiTab (TabInt T)
-/* Proses : Menuliskan isi tabel dengan traversal, tabel ditulis di antara kurung siku; 
-   antara dua elemen dipisahkan dengan separator "koma", tanpa tambahan karakter di depan,
-   di tengah, atau di belakang, termasuk spasi dan enter */
-/* I.S. T boleh kosong */
-/* F.S. Jika T tidak kosong: [e1,e2,...,en] */
-/* Contoh : jika ada tiga elemen bernilai 1, 20, 30 akan dicetak: [1,20,30] */
-/* Jika tabel kosong : menulis [] */
-{
-	//KAMUS LOKAL
-	
-	//ALGORITMA
-	
-}
-
-/* ********** MENAMBAH ELEMEN ********** */
-/* *** Menambahkan elemen terakhir *** */
-void AddAsLastEl (TabInt *T, ElType X)
-/* Proses: Menambahkan X sebagai elemen terakhir tabel */
-/* I.S. Tabel T boleh kosong, tetapi tidak penuh */
-/* F.S. X adalah elemen terakhir T yang baru */
-{
-	//KAMUS LOKAL
-	
-	//ALGORITMA
-	
-}
-void AddEli (TabInt *T, ElType X, IdxType i)
-/* Menambahkan X sebagai elemen ke-i tabel tanpa mengganggu kontiguitas 
-   terhadap elemen yang sudah ada */
-/* I.S. Tabel tidak kosong dan tidak penuh */
-/*      i adalah indeks yang valid. */
-/* F.S. X adalah elemen ke-i T yang baru */
-/* Proses : Geser elemen ke-i+1 s.d. terakhir */
-/*          Isi elemen ke-i dengan X */
-{
-	//KAMUS LOKAL
-	
-	//ALGORITMA
-	
-}
-
-/* ********** MENGHAPUS ELEMEN ********** */
-void DelLastEl (TabInt *T, ElType * X)
-/* Proses : Menghapus elemen terakhir tabel */
-/* I.S. Tabel tidak kosong */
-/* F.S. X adalah nilai elemen terakhir T sebelum penghapusan, */
-/*      Banyaknya elemen tabel berkurang satu */
-/*      Tabel T mungkin menjadi kosong */
-{
-	//KAMUS LOKAL
-	
-	//ALGORITMA
-	
-}
-void DelEli (TabInt *T, IdxType i, ElType * X)
+void DelEli (TabInt *T, IdxType i, ElType *X)
 /* Menghapus elemen ke-i tabel tanpa mengganggu kontiguitas */
 /* I.S. Tabel tidak kosong, i adalah indeks efektif yang valid */
 /* F.S. X adalah nilai elemen ke-i T sebelum penghapusan */
@@ -240,8 +69,17 @@ void DelEli (TabInt *T, IdxType i, ElType * X)
 /* Proses : Geser elemen ke-i+1 s.d. elemen terakhir */
 /*          Kurangi elemen efektif tabel */
 {
-	//KAMUS LOKAL
-	
-	//ALGORITMA
-	
+	//Kamus
+	IdxType j;
+
+	//Algoritma
+	*X = Elmt(*T, i);
+	if (IsEmpty(*T) == false)
+	{
+		for(j = i; j < GetLastIdx(*T); j++)
+		{
+			Elmt(*T, j) = Elmt(*T, (j + 1));
+		}
+		Neff(*T) -= 1;
+	}
 }
