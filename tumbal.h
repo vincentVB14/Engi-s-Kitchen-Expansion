fff290ffd43a98b28f9247587726563f95422ade
Nixon Andhika <43450143+hidx1@users.noreply.github.com>
2018-11-24 16:00:42 +0700

    Delete tumbal.h

--- tumbal.h
+++
@@ -1,9 +0,0 @@
-// tumbal.h
-
-int FakeStrCmp (char* str1, char* str2);
-
-int FakeStrNCmp (char* str1, char* str2, int N);
-
-void FakeStrCpy (char* str1, char* str2);
-
-int FakeAtoi (char* str1);
