200b4169e27cca570f10708c3724f7971b63f5fc
Ferdy Santoso <36350020+ferdysan360@users.noreply.github.com>
2018-11-24 11:52:38 +0700

    CreateFood (Bintree -> BinTree)

--- tumbal.c
+++ tumbal.c
@@ -3,72 +3,72 @@
 #include "boolean.h"
 #include "tumbal.h"
 
-int main()
-{
-  // KAMUS
-  char str1[20];
-  char str2[20];
-  boolean same;
-  int choice;
-  int chars;
+// int main()
+// {
+//   // KAMUS
+//   char str1[20];
+//   char str2[20];
+//   boolean same;
+//   int choice;
+//   int chars;
 
-  // ALGORITMA
-  printf("Choices ....\n1. FakeStrCmp\n2. FakeStrNCmp\n3. FakeStrCpy\n4. FakeAtoi\n5. Exit\n");
-  printf("Choice = ");
-  scanf("%d", &choice);
+//   // ALGORITMA
+//   printf("Choices ....\n1. FakeStrCmp\n2. FakeStrNCmp\n3. FakeStrCpy\n4. FakeAtoi\n5. Exit\n");
+//   printf("Choice = ");
+//   scanf("%d", &choice);
 
-  while (choice != 5)
-  {
-    switch (choice) {
-      case 1:
-        printf("Str 1 = ");
-        scanf("%s", str1);
-        printf("Str 2 = ");
-        scanf("%s", str2);
-        if (FakeStrCmp(str1, str2) == 0)
-        {
-          printf("Kedua string sama\n");
-        }
-        else
-        {
-          printf("Kedua string tidak sama\n");
-        }
-        break;
-      case 2:
-        printf("Str 1 = ");
-        scanf("%s", str1);
-        printf("Str 2 = ");
-        scanf("%s", str2);
-        printf("Berapa banyak char : ");
-        scanf("%d", &chars);
-        if (FakeStrNCmp(str1, str2, chars) == 0)
-        {
-          printf("Kedua string sama\n");
-        }
-        else
-        {
-          printf("Kedua string tidak sama\n");
-        }
-        break;
-      case 3:
-        printf("Str yang ingin dicopy : ");
-        scanf("%s", str1);
-        FakeStrCpy(str1, str2);
-        printf("Hasil kopian : %s\n", str2);
-        break;
-      case 4:
-        printf("Str yang ingin diubah ke integer : ");
-        scanf("%s", str1);
-        printf("Hasil integernya : %d\n", FakeAtoi(str1));
-        break;
-      default:
-        printf("Wrong input. Try again\n");
-    }
-    printf("Choices ....\n1. FakeStrCmp\n2. FakeStrNCmp\n3. FakeStrCpy\n4. FakeAtoi\n5. Exit\n");
-    printf("Choice = ");
-    scanf("%d", &choice);
-  }
-}
+//   while (choice != 5)
+//   {
+//     switch (choice) {
+//       case 1:
+//         printf("Str 1 = ");
+//         scanf("%s", str1);
+//         printf("Str 2 = ");
+//         scanf("%s", str2);
+//         if (FakeStrCmp(str1, str2) == 0)
+//         {
+//           printf("Kedua string sama\n");
+//         }
+//         else
+//         {
+//           printf("Kedua string tidak sama\n");
+//         }
+//         break;
+//       case 2:
+//         printf("Str 1 = ");
+//         scanf("%s", str1);
+//         printf("Str 2 = ");
+//         scanf("%s", str2);
+//         printf("Berapa banyak char : ");
+//         scanf("%d", &chars);
+//         if (FakeStrNCmp(str1, str2, chars) == 0)
+//         {
+//           printf("Kedua string sama\n");
+//         }
+//         else
+//         {
+//           printf("Kedua string tidak sama\n");
+//         }
+//         break;
+//       case 3:
+//         printf("Str yang ingin dicopy : ");
+//         scanf("%s", str1);
+//         FakeStrCpy(str1, str2);
+//         printf("Hasil kopian : %s\n", str2);
+//         break;
+//       case 4:
+//         printf("Str yang ingin diubah ke integer : ");
+//         scanf("%s", str1);
+//         printf("Hasil integernya : %d\n", FakeAtoi(str1));
+//         break;
+//       default:
+//         printf("Wrong input. Try again\n");
+//     }
+//     printf("Choices ....\n1. FakeStrCmp\n2. FakeStrNCmp\n3. FakeStrCpy\n4. FakeAtoi\n5. Exit\n");
+//     printf("Choice = ");
+//     scanf("%d", &choice);
+//   }
+// }
 
 int FakeStrCmp (char* str1, char* str2)
 {
